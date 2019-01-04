bd3dde9bbc1a5c1f8ea37b759d4f5f5227437abc
ballpot00369 <ballpot00362@gmail.com>
2019-01-04 16:03:27 +0700

    HW

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 let students = [
-    {
-      "id": 4266240094,
-      "name": "nitigan nakjuatong",
-      "username_github": "nitigan",
-      "gender": "male",
-      "age": 39
-    }
-  ]
+  {
+    "id": 4266240094,
+    "name": "nitigan nakjuatong",
+    "username_github": "nitigan",
+    "gender": "male",
+    "age": 39
+  },
+  {
+    "id": 5906021623030,
+    "name": "sutthichai rattanaamphan",
+    "username_github": "ballpot00147",
+    "gender": "male",
+    "age": 20
+  }
+]
 
-  console.log(students);
+console.log(students);
