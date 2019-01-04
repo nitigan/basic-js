da04aeec2608988d71e7cdfd21b7ed06d0d34229
nitigan <nitigan.n@fitm.kmutnb.ac.th>
2019-01-04 15:16:45 +0700

    editName

--- index.js
+++ index.js
@@ -1,61 +1,11 @@
-/*
-let users = [
+let students = [
     {
-      "id": 1000,
-      "name": "Corey Griffith",
-      "username": "coGriffith",
+      "id": 4266240094,
+      "name": "nitigan nakjuatong",
+      "username_github": "nitigan",
       "gender": "male",
-      "age": 13
-    },
-    {
-      "id": 1001,
-      "name": "Marion McDaniel",
-      "username": "mMcDaniel",
-      "gender": "female",
-      "age": 15
-    },
-    {
-      "id": 1002,
-      "name": "Tom Robbins",
-      "username": "tRobbins",
-      "gender": "male",
-      "age": 15
-    },
-    {
-      "id": 1003,
-      "name": "Ruth Harvey",
-      "username": "rHarvey",
-      "gender": "female",
-      "age": 14
-    },
-    {
-      "id": 1004,
-      "name": "Terry Willis",
-      "username": "tWillis",
-      "gender": "male",
-      "age": 13
+      "age": 39
     }
   ]
 
-console.log(users[0]);
-*/
-/*
-function main () {
-    plusNumber1(55, 10, function(callback) {
-      console.log(callback) // 15
-    })
-    console.log(plusNumber2(3,7))
-  }
-  function plusNumber1(num1, num2, callback) {
-    callback(num1+num2)
-  }
-  function plusNumber2(num1, num2) {
-    return num1+num2
-  }
-  main() // log 15 จากนั้น log 10
-  */
-
- setTimeout(function () {
-    console.log('first')
-   }, 100)
-   console.log('second') //log second ก่อนแล้ว log first
+  console.log(students);
